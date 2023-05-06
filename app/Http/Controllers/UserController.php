d167afac381e44449d0a5a81fd18cb44d0415561
CTO <thepropertymanager2020@gmail.com>
2023-05-06 15:02:05 +0800

    added the controllers and routes

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function index()
+    {
+        return User::all();
+
+        //select * from users;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        ddd('this is user create page');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        ddd('this is user store page');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return User::find($id);
+
+        //select * from users where id = $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        ddd('this is user edit page');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        ddd('asdasd');
+
+       return User::where('id', $id)
+       ->update([
+        'name' => $request->name
+       ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        ddd('this is user destroy page');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\CommentController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +20,37 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//routes for the user 
+Route::controller(UserController::class)->group(function () {
+  
+   Route::get('/users', 'index');
+
+   Route::get('/users/create', 'create');
+
+   Route::post('/users/store', 'store');
+
+   Route::get('/users/{id}', 'show')->whereNumber('id');
+
+   Route::get('/users/{id}/edit', 'edit')->whereNumber('id');
+
+   Route::patch('/users/{id}/update', 'update')->whereNumber('id');
+
+   Route::delete('/users/{id}/delete', 'destroy')->whereNumber('id');
+});
+
+//routes for the blog
+Route::controller(BlogController::class)->group(function () {
+    //routes here
+});
+
+//routes for the like
+Route::controller(LikeController::class)->group(function () {
+    //routes here
+});
+
+//routes for the comment
+Route::controller(CommentController::class)->group(function () {
+    //routes here
 });
